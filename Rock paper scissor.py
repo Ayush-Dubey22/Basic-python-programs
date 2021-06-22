7884a20d4ef88f7b3f3cc07d9978a747525ef651
Ayush-Dubey22 <84781894+Ayush-Dubey22@users.noreply.github.com>
2021-06-22 12:37:21 +0530

    Add files via upload

---
+++ Rock paper scissor.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+
+list = ['Rock', 'Paper', 'Scissor']
+
+
+a = input('Determin Your move: ').lower
+print('Rock'),time.sleep(0.5)
+print('Paper'),time.sleep(0.5)
+print('Scissor'),time.sleep(0.5)
+print('Shoot!\n'),time.sleep(0.3)
+b = random.choice(list)
+print(b)
+
+ 
